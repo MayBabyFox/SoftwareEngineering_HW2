12cfdc61d0fe2cd05717aac2c41a440038be1101
Елизавета <erlomakina@edu.hse.ru>
2026-06-03 02:29:44 +0300

    Добавлен класс Recipe. Реализованы методы: 1) __init__ , инициализирующий атрибуты title и ingredients; 2) add_ingredient , добавляющий ингредиенты в рецепт; 3) is_valid_ratio; 4) scale; 5) __len__ , возвращающий количество уникальных ингредиентов; 6) __str__ , возвращающий название блюда и список ингредиентов;

---
+++ Recipe.py
@@ -0,0 +1,39 @@
+from Ingredient import Ingredient
+
+
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = []
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for component in self.ingredients:
+            if component == ingredient:
+                component.quantity += ingredient.quantity
+                return
+            self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if ratio > 0:
+            return True
+        return False
+
+    def scale(self, ratio: float):
+        new_ingredients = []
+        for component in self.ingredients:
+            new_component = (Ingredient(component.name, component.quantity * ratio, component.unit))
+            new_ingredients.append(new_component)
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self):
+        unique_components = []
+        for component in self.ingredients:
+            if not (component.name, component.unit) in unique_components:
+                unique_components.append(component.name)
+        return len(unique_components)
+
+    def __str__(self):
+        ingredients_reader = []
+        for component in self.ingredients:
+            ingredients_reader.append(f"{component.name}: {component.quantity} {component.unit}")
